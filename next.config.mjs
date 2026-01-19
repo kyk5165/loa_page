7ca88b6ebfea605f8feb815296161877117fde62
kyk5165 <kyk5165@naver.com>
2026-01-20 01:20:55 +0900

    feat(next.config.mjs): API URL을 환경 변수에 따라 동적으로 설정 chore(next.config.mjs): 개발 및 프로덕션 환경에 맞는 API 리다이렉트 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
+    const apiUrl = process.env.NODE_ENV === 'production'
+      ? 'https://loa-cloud-api.dbzos321.workers.dev'
+      : 'http://localhost:8787';
+
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:8787/api/:path*',
+        destination: `${apiUrl}/api/:path*`,
       },
     ];
   },
