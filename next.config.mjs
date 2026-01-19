9162c7c2323ccd591f2d4c5c298b87637df3dd90
kyk5165 <kyk5165@naver.com>
2026-01-20 01:21:11 +0900

    fix(next.config.mjs): API URL 설정을 환경 변수로 변경 chore(next.config.mjs): 개발 환경에서의 API URL을 환경 변수로 관리

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
-    const apiUrl = process.env.NODE_ENV === 'production'
-      ? 'https://loa-cloud-api.dbzos321.workers.dev'
-      : 'http://localhost:8787';
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8787';
 
     return [
       {
